149b219ae1693cc4185c26dabac824c68630282f
Celo Reis <marcelo@tryalma.ai>
2024-08-14 11:57:13 -0300

    feat: Basic Implementation

---
+++ api/polygon.ts
@@ -0,0 +1,33 @@
+import { Axios } from "axios";
+
+const axios = new Axios({
+  baseURL: "https://api.polygon.io/",
+});
+
+axios.interceptors.request.use((config) => {
+  config.url += `&apiKey=${process.env.POLYGON_API_KEY}`;
+  return config;
+});
+
+type StockResponse = {
+  status: "OK";
+  from: string;
+  symbol: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  afterHours: number;
+  preMarket: number;
+};
+
+export async function getStock(stockSymbol: string) {
+  const { data } = await axios.get<StockResponse>(
+    `/v1/open-close/${stockSymbol}/${
+      new Date(Date.now() - 1000 * 60 * 60 * 24).toISOString().split("T")[0]
+    }/?adjusted=true`
+  );
+
+  return data;
+}
--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+import "dotenv/config";
+
+import { getStock } from "./api/polygon";
+
+const stocks = ["AAPL"];
+
+const main = async () => {
+  for (const stock of stocks) {
+    const stockData = await getStock(stock);
+    console.log(stockData);
+  }
+};
+
+main();
